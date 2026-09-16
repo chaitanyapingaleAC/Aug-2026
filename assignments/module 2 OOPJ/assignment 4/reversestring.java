fc2f76e58f3ea9073f8350ef275e8e7dbd7aec86
Chaitanya Pingale <chaitanya.pingale.cmaug26@gmail.com>
2026-09-16 18:30:26 +0530

    Create reversestring.java

---
+++ assignments/module 2 OOPJ/assignment 4/reversestring.java
@@ -0,0 +1,24 @@
+package day9;
+
+public class stringclass {
+
+	public static void main(String[] args) {
+		
+		String s = "The Quick BroWn FoX!";
+
+
+		for(int c=s.length()-1;c>=0;c--) {
+			System.out.print(s.charAt(c));
+		}
+		
+		
+		
+		
+	
+		}
+
+		
+
+	}
+
+
