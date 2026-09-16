4b8b917cd84df1c5b62a500654cbddfef023abbe
Chaitanya Pingale <chaitanya.pingale.cmaug26@gmail.com>
2026-09-16 17:40:54 +0530

    Create stringend.java

---
+++ assignments/module 2 OOPJ/assignment 4/stringend.java
@@ -0,0 +1,18 @@
+package day9;
+
+public class stringclass {
+
+	public static void main(String[] args) {
+		
+		String s="java exersise";
+		String ss="java exersises";
+		
+		
+		System.out.println(s.endsWith("se"));
+		System.out.println(ss.endsWith("se"));
+
+		
+
+	}
+
+}
