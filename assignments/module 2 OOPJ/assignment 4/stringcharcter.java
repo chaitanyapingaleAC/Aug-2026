6f34a5579965f42a3e01d5b5af3cd6f6e0f6fb2d
Chaitanya Pingale <chaitanya.pingale.cmaug26@gmail.com>
2026-09-16 16:12:10 +0530

    Create stringcharcter.java

---
+++ assignments/module 2 OOPJ/assignment 4/stringcharcter.java
@@ -0,0 +1,17 @@
+package day9;
+
+public class stringclass {
+
+	public static void main(String[] args) {
+		
+		String s="java exersise!";
+		System.out.println(s);
+		System.out.println("char at 0 is  : "+s.charAt(0));
+		System.out.println("char at 10 is  : "+s.charAt(10));
+		
+		
+		
+
+	}
+
+}
