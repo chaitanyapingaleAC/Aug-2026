ab5cb717e5283e1a9eb732af54e475427ec751e4
Chaitanya Pingale <chaitanya.pingale.cmaug26@gmail.com>
2026-09-16 18:06:03 +0530

    Create stringreplace.java

---
+++ assignments/module 2 OOPJ/assignment 4/stringreplace.java
@@ -0,0 +1,20 @@
+package day9;
+
+public class stringclass {
+
+	public static void main(String[] args) {
+		
+		String s = "The quick brown fox jumps over the lazy dog.";
+
+	
+		System.out.println(s);
+		String ss=s.replaceAll("fox", "cat");
+		System.out.println(ss);
+			
+		}
+
+		
+
+	}
+
+
