2e1d80e32eed646e0efb1843c3180394987c9274
Chaitanya Pingale <chaitanya.pingale.cmaug26@gmail.com>
2026-09-16 18:26:00 +0530

    Create string uppercase.java

---
+++ assignments/module 2 OOPJ/assignment 4/string uppercase.java
@@ -0,0 +1,17 @@
+package day9;
+
+public class stringclass {
+
+	public static void main(String[] args) {
+		
+		String s = "The Quick BroWn FoX!";
+
+		System.out.println(s.toUpperCase());
+	
+		}
+
+		
+
+	}
+
+
